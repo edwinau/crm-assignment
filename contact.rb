2fd7acaeac3334f239375ff33feae7e327e6fd26
EDWIN AU <edwinau@rogers.com>
2017-04-02 21:02:25 -0400

    crm added attributes

--- contact.rb
+++ contact.rb
@@ -1,30 +1,26 @@
 class Contact
- @@display_all = {}
- @add = add
- @modify = modify
- @delete = delete
- @search_by_attribute = search_by_attribute
- @exit = exit
+ # @@display_all = {}
+ # @add = add
+ # @modify = modify
+ # @delete = delete
+ # @search_by_attribute = search_by_attribute
+ # @exit = exit
 
-  attr_reader :exit
-  attr_accessor :add, :modify, :delete, :first_name, :display_all, :search_by_attribute; first
+  attr_reader :id
+  attr_accessor :add, :modify, :delete, :first_name, :display_all, :search_by_attribute, :first_name, :last_name, :email, :note
 
   # This method should initialize the contact's attributes
-  def initialize
-    puts "please choose from the following"
-    puts " - add"
-    puts " - modify"
-    puts " - delete"
-    puts " - display_all"
-    puts " - search by attribute"
-    puts " - exit"
-    gets.chomp!
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
   end
 
   # This method should call the initializer,
   # store the newly created contact, and then return it
   def self.create
-    x = Contact.new
+    x = Contact.new(first_name, last_name, email, note)
     @@display_all << x
     return x
   end
@@ -33,12 +29,6 @@ class Contact
   # This method should return all of the existing contacts
   def self.all
     puts @@display_all
-  end
-
-  def add(first_name, last_name, email, note)
-    @first_name = first_name
-    @last_name = last_name
-
   end
 
   # This method should accept an id as an argument
@@ -81,3 +71,22 @@ class Contact
   # Feel free to add other methods here, if you need them.
 
 end
+
+
+
+
+
+
+
+
+
+
+
+# puts "please choose from the following"
+# puts " - add"
+# puts " - modify"
+# puts " - delete"
+# puts " - display_all"
+# puts " - search by attribute"
+# puts " - exit"
+# gets.chomp!
